387182d46ecdeeaa0206aed6d5aca96fa9eca733
abdo99ibrahim <abdelramin30@gmail.com>
2022-12-11 13:11:30 +0200

    first commit

---
+++ vue/src/store/index.js
@@ -0,0 +1,14 @@
+import {createStore} from 'vuex';
+const store = createStore({
+    state:{
+        user:{
+          data:{name: ' ahmed'},
+          token: null
+        },
+    },
+    getters:{},
+    actions:{},
+    mutations: {},
+    modules:{},
+});
+export default store;
