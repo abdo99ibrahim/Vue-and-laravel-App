c76e1e41ec44986f45eda98a5d4e276b85b16f1a
abdo99ibrahim <abdelramin30@gmail.com>
2022-12-15 16:00:01 +0200

    work Done: ========== 1. create SurveyView view for show and editing 2. create QuestionSurvey component as child showing in SurveyView Component to display questions 3.  define 2 routes   -> /surveys/create :::::: for create   -> /surveys/:id    :::::: for view 4. in store/index.js  -> define variable called tmpSurveys that carry array of objects 5. define slot tag in page component and named to header 6. install uuid package For the creation of RFC4122 UUIDs

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -5,6 +5,7 @@ import Login from "../views/Login.vue";
 import Dashboard from "../views/Dashboard.vue";
 import Register from "../views/Register.vue";
 import Surveys from "../views/Surveys.vue";
+import SurveyView from "../views/SurveyView.vue";
 import store from './../store/index';
 const routes = [
   {
@@ -23,6 +24,16 @@ const routes = [
             name: "Surveys",
             component: Surveys,
         },
+        {
+            path: "/surveys/create",
+            name: "SurveyCreate",
+            component: SurveyView,
+        },
+        {
+          path: "/surveys/:id",
+          name: "SurveyView",
+          component: SurveyView,
+      },
     ],
   },
   {
